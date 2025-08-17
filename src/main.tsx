f844b247fd9ba86948a505cec4c0ffaea3b50741
Mark Stephenson <helpme@healthandtravels.com>
2025-08-16 20:10:44 -0700

    stabilize vite baseline for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-﻿import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+﻿import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
 
-const el = document.getElementById('root')!
+const el = document.getElementById("root")!
 createRoot(el).render(<App />)
